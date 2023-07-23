0a6a118ddf9a76e0b49e48394af72ecbc96c7365
Jade Park <dev.jadepark@gmail.com>
2023-07-23 20:09:05 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import DateHead from './components/DateHead';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <DateHead />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;

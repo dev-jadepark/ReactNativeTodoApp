34660be04c94137d37daa6e01de9e460d6c2f2c6
dev-jadepark <dev.jadepark@gmail.com>
2023-07-30 13:28:32 +0900

    할일 UI 및 Platform select테스트

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet, View} from 'react-native';
 import DateHead from './components/DateHead';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 
 function App() {
+  const today = new Date();
+  console.log(today);
+
+
   return (
-    <SafeAreaView>
-      <View>
-        <DateHead />
-      </View>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <KeyboardAvoidingView
+          behavior={Platform.select({ios: 'padding', android: undefined})}
+          style={styles.avoid}>
+          <DateHead date={today} />
+          <Empty />
+          <AddTodo />
+        </KeyboardAvoidingView>
+      </SafeAreaView> 
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  avoid: {
+    flex: 1,
+  }
+});
 
 export default App;
